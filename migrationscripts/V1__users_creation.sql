789153632525af3b4c71a94ac0c8d32f242c6605
Haitham.Said <haitham.soliman94@gmail.com>
2025-04-12 14:27:21 +0400

    creating notification table

--- migrationscripts/V1__users_creation.sql
+++ migrationscripts/V1__users_creation.sql
@@ -1,5 +1,15 @@
+CREATE TABLE companies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255),
+    phone VARCHAR(20),
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
+    company_id INT REFERENCES companies(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR(20) UNIQUE NOT NULL,
@@ -8,9 +18,6 @@ CREATE TABLE users (
     status VARCHAR(20) CHECK (status IN ('ACTIVE', 'INACTIVE')) DEFAULT 'ACTIVE',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
--- Ensure only one admin exists (Optional, uncomment if needed)
--- CREATE UNIQUE INDEX unique_admin ON users (role) WHERE role = 'ADMIN';
 
 CREATE TABLE maintainers (
     user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
@@ -30,4 +37,29 @@ CREATE TABLE maintainer_skillset (
     maintainer_id INT REFERENCES maintainers(user_id) ON DELETE CASCADE,
     skillset_id INT REFERENCES skillsets(id) ON DELETE CASCADE,
     PRIMARY KEY (maintainer_id, skillset_id)
+);
+
+
+
+CREATE TABLE buildings (
+    id SERIAL PRIMARY KEY,
+    company_id INT REFERENCES companies(id) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE apartments (
+    id SERIAL PRIMARY KEY,
+    building_id INT REFERENCES buildings(id) ON DELETE CASCADE,
+    apartment_number VARCHAR(50) NOT NULL,
+    floor_number INT,
+    UNIQUE (building_id, apartment_number)
+);
+
+CREATE TABLE customers (
+    user_id INT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    apartment_id INT REFERENCES apartments(id) ON DELETE SET NULL,
+    -- other potential fields
+    move_in_date DATE
 );
