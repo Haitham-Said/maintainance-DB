4dc9a5a71a58332a70b716fce11a29b6440abbcc
Haitham Said <haitham.soliman94@gmail.com>
2025-07-04 18:24:05 +0400

    change create_at to updated_at && change preffered_date to string

--- migrationscripts/V5__maintainance_request_update.sql
+++ migrationscripts/V5__maintainance_request_update.sql
@@ -1,3 +1,3 @@
-ALTER TABLE maintainance_request ALTER COLUMN preferred_time TYPE VARCHAR(64);
+ALTER TABLE maintenance_requests ALTER COLUMN preferred_time TYPE VARCHAR(64);
 
-ALTER TABLE maintainance_request RENAME COLUMN created_at TO updated_at;
+ALTER TABLE maintenance_requests RENAME COLUMN created_at TO updated_at;
